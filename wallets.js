4d74a4015b8036c1b45339c0298df6529b5e079e
Kaspa Hub <ivoclassic@pm.me>
2025-07-25 21:51:12 +0300

    Create wallets.js

---
+++ wallets.js
@@ -0,0 +1,13 @@
+// List of users with associated X.com username, Kaspa wallet address, and optional KNS domain.
+export const users = [
+{
+	x: "KaspaHub",
+	wallet: "kaspa:qrv8305twny4434fjnsqvk2875tk3g27flz5akm49rzd7gpk0lrsus8y3ahwm",
+	domain: "kas-hub.kas"
+},
+{
+	x: "Pumpolinsky",
+	wallet: "kaspa:qr408tqlvgc75skyp3zztqrhkrdg2584qkzg30t9kt2nllryzzwxqx5t83q5w",
+	domain: "pumpolinsky.kas"
+}
+];
