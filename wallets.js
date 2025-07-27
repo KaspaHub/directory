c07f48b99b291c7f6018affa132f791e2787fc73
Kaspa Hub <ivoclassic@pm.me>
2025-07-27 14:10:12 +0300

    Update wallets.js

--- wallets.js
+++ wallets.js
@@ -1,17 +1,21 @@
-export const users = {
-KaspaHub: {
+export const users = [
+{
+	x: "KaspaHub",
 	wallet: "kaspa:qrv8305twny4434fjnsqvk2875tk3g27flz5akm49rzd7gpk0lrsus8y3ahwm",
 	domain: "kas-hub.kas"
 },
-KaspaSilver: {
+{
+	x: "KaspaSilver",
 	wallet: "kaspa:qp408svlz585vyvj50yaljm8xdxrkcmmed8vxlx0wf0cl5wpt3vzyh74xs46e",
 	domain: "kaspasilver.kas"
 },
-Pumpolinsky: {
+{
+	x: "Pumpolinsky",
 	wallet: "kaspa:qr408tqlvgc75skyp3zztqrhkrdg2584qkzg30t9kt2nllryzzwxqx5t83q5w",
 	domain: "pumpolinsky.kas"
 },
-IzioDev: {
-	wallet: "kaspa:qpmwdyku2fcwzcahdzs86d0zz606vy7y6qxgrpkjxdz5xz8z6af9w2u6w0f3t"
+{
+	x: "IzioDev",
+	wallet: "kaspa:qpmwdyku2fcwzcahdzs86d0zz606vy7y6qxgrpkjxdz5xz8z6af9w2u6w0f3t",
 }
-};
+];
