20fe12b661847e98f9341de24c2cd77c0e6edac7
Kaspa Hub <ivoclassic@pm.me>
2025-07-27 23:51:08 +0300

    Update users.js

--- users.js
+++ users.js
@@ -1,26 +1,26 @@
 export const users = [
 {
-	x: "KaspaHub",
-	wallet: "kaspa:qrv8305twny4434fjnsqvk2875tk3g27flz5akm49rzd7gpk0lrsus8y3ahwm",
-	domain: "kas-hub.kas"
+x: "KaspaHub",
+wallet: "kaspa:qrv8305twny4434fjnsqvk2875tk3g27flz5akm49rzd7gpk0lrsus8y3ahwm",
+domain: "kas-hub.kas"
 },
 {
-	x: "KaspaSilver",
-	wallet: "kaspa:qp408svlz585vyvj50yaljm8xdxrkcmmed8vxlx0wf0cl5wpt3vzyh74xs46e",
-	domain: "kaspasilver.kas"
+x: "KaspaSilver",
+wallet: "kaspa:qp408svlz585vyvj50yaljm8xdxrkcmmed8vxlx0wf0cl5wpt3vzyh74xs46e",
+domain: "kaspasilver.kas"
 },
 {
-	x: "Pumpolinsky",
-	wallet: "kaspa:qr408tqlvgc75skyp3zztqrhkrdg2584qkzg30t9kt2nllryzzwxqx5t83q5w",
-	domain: "pumpolinsky.kas"
+x: "Pumpolinsky",
+wallet: "kaspa:qr408tqlvgc75skyp3zztqrhkrdg2584qkzg30t9kt2nllryzzwxqx5t83q5w",
+domain: "pumpolinsky.kas"
 },
 {
-	x: "IzioDev",
-	wallet: "kaspa:qpmwdyku2fcwzcahdzs86d0zz606vy7y6qxgrpkjxdz5xz8z6af9w2u6w0f3t",
+x: "IzioDev",
+wallet: "kaspa:qpmwdyku2fcwzcahdzs86d0zz606vy7y6qxgrpkjxdz5xz8z6af9w2u6w0f3t",
 },
 {
-	x: "coinathlete",
-	wallet: "kaspa:qq6vraj894yeps2xltuzjllhdj85fsc5wfs4wz53hc4e4v0yhvtcx7s6e4fvr",
-	domain: "coinathlete.kas"
+x: "coinathlete",
+wallet: "kaspa:qq6vraj894yeps2xltuzjllhdj85fsc5wfs4wz53hc4e4v0yhvtcx7s6e4fvr",
+domain: "coinathlete.kas"
 }
 ];
