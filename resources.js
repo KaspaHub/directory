ca5b236e549f4bce45fd9cfd8301026bf79000db
Kaspa Hub <ivoclassic@pm.me>
2025-07-27 15:02:14 +0300

    Create resources.js

---
+++ resources.js
@@ -0,0 +1,435 @@
+export const users = [
+{
+  name: "PHANTOM GHOSTDAG Whitepaper",
+  officialSite: "https://eprint.iacr.org/2018/104.pdf",
+  logo: "https://cdn.jsdelivr.net/gh/KaspaHub/KaspaHub@latest/assets/kaspa.webp",
+  features: {
+  }
+},
+{
+  name: "DAG KNIGHT Whitepaper",
+  officialSite: "https://eprint.iacr.org/2022/1494.pdf",
+  logo: "https://cdn.jsdelivr.net/gh/KaspaHub/KaspaHub@latest/assets/kaspa.webp",
+  features: {
+  }
+},
+{
+  name: "Kaspa.org",
+  officialSite: "https://kaspa.org",
+  logo: "https://cdn.jsdelivr.net/gh/KaspaHub/KaspaHub@latest/assets/kaspa.webp",
+  features: {
+    articles: true,
+    videos: true
+  }
+},
+{
+  name: "Kaspa.com",
+  officialSite: "https://kaspa.com/learn-kaspa",
+  logo: "https://cdn.jsdelivr.net/gh/KaspaHub/KaspaHub@latest/assets/logos/kaspacom.webp",
+  features: {
+    articles: true
+  }
+},
+{
+  name: "KASmedia",
+  officialSite: "https://www.kasmedia.com",
+  logo: "https://cdn.jsdelivr.net/gh/KaspaHub/KaspaHub@latest/assets/logos/kasmedia.webp",
+  features: {
+    articles: true
+  }
+},
+{
+  name: "Kaspa Ecosystem Foundation",
+  officialSite: "https://www.kaspafoundation.org",
+  logo: "https://cdn.jsdelivr.net/gh/KaspaHub/KaspaHub@latest/assets/logos/kef.webp",
+  features: {
+    videos: true
+  }
+},
+{
+  name: "Kaspa Industrial Initiative",
+  officialSite: "https://kaspa-kii.org",
+  logo: "https://cdn.jsdelivr.net/gh/KaspaHub/KaspaHub@latest/assets/logos/kii.webp",
+  features: {
+    videos: true
+  }
+},
+{
+  name: "Kaspa Research",
+  officialSite: "https://research.kas.pa",
+  logo: "https://cdn.jsdelivr.net/gh/KaspaHub/KaspaHub@latest/assets/logos/kasparesearch.webp",
+  features: {
+  }
+},
+{
+  name: "Kaspa Wiki",
+  officialSite: "https://wiki.kaspa.org",
+  logo: "https://cdn.jsdelivr.net/gh/KaspaHub/KaspaHub@latest/assets/logos/kaspawiki.webp",
+  features: {
+  }
+},
+{
+  name: "Yonatan Sompolinsky Medium",
+  officialSite: "https://hashdag.medium.com",
+  logo: "https://cdn.jsdelivr.net/gh/KaspaHub/KaspaHub@latest/assets/logos/medium.webp",
+  features: {
+    articles: true
+  }
+},
+{
+  name: "Shai Wyborski Medium",
+  officialSite: "https://medium.com/@shai.wyborski",
+  logo: "https://cdn.jsdelivr.net/gh/KaspaHub/KaspaHub@latest/assets/logos/medium.webp",
+  features: {
+    articles: true
+  }
+},
+{
+  name: "Michael Sutton Medium",
+  officialSite: "https://medium.com/@michaelsuttonil",
+  logo: "https://cdn.jsdelivr.net/gh/KaspaHub/KaspaHub@latest/assets/logos/medium.webp",
+  features: {
+    articles: true
+  }
+},
+{
+  name: "Aviv Zohar Medium",
+  officialSite: "https://medium.com/@avivzohar",
+  logo: "https://cdn.jsdelivr.net/gh/KaspaHub/KaspaHub@latest/assets/logos/medium.webp",
+  features: {
+    articles: true
+  }
+},
+{
+  name: "Ori Newman Medium",
+  officialSite: "https://someone235.medium.com/",
+  logo: "https://cdn.jsdelivr.net/gh/KaspaHub/KaspaHub@latest/assets/logos/medium.webp",
+  features: {
+    articles: true
+  }
+},
+{
+  name: "coderofstuff Medium",
+  officialSite: "https://medium.com/@coderofstuff",
+  logo: "https://cdn.jsdelivr.net/gh/KaspaHub/KaspaHub@latest/assets/logos/medium.webp",
+  features: {
+    articles: true
+  }
+},
+{
+  name: "Kaspa Core R&D (public) Telegram",
+  officialSite: "https://t.me/kasparnd",
+  logo: "https://cdn.jsdelivr.net/gh/KaspaHub/KaspaHub@latest/assets/logos/telegram.webp",
+  features: {
+  }
+},
+{
+  name: "KaspaSilver",
+  officialSite: "https://linktr.ee/KaspaSilver",
+  logo: "https://cdn.jsdelivr.net/gh/KaspaHub/KaspaHub@latest/assets/logos/kaspasilver.webp",
+  features: {
+    articles: true,
+    videos: true
+  }
+},
+{
+    name: "The Book of Kaspa",
+    officialSite: "https://www.amazon.com/Book-Kaspa-Realizing-Nakamoto-Dream-ebook/dp/B0CCVTFM2N",
+    logo: "https://cdn.jsdelivr.net/gh/KaspaHub/KaspaHub@latest/assets/kaspa.webp",
+    features: {
+    }
+},
+{
+  name: "KaspArchive",
+  officialSite: "https://kasparchive.com",
+  logo: "https://cdn.jsdelivr.net/gh/KaspaHub/KaspaHub@latest/assets/kaspa.webp",
+  features: {
+  }
+},
+{
+  name: "Kaspa Daily",
+  officialSite: "https://kaspadaily.com/",
+  logo: "https://cdn.jsdelivr.net/gh/KaspaHub/KaspaHub@latest/assets/logos/kaspadaily.webp",
+  features: {
+    articles: true
+  }
+},
+{
+  name: "Kasbun",
+  officialSite: "https://kasbun.vercel.app/",
+  logo: "https://cdn.jsdelivr.net/gh/KaspaHub/KaspaHub@latest/assets/logos/kasbun.webp",
+  features: {
+    articles: true
+  }
+},
+{
+  name: "Mineable Money",
+  officialSite: "https://mineable.money",
+  logo: "https://cdn.jsdelivr.net/gh/KaspaHub/KaspaHub@latest/assets/logos/mineablemoney.webp",
+  features: {
+  }
+},
+{
+  name: "KasBay",
+  officialSite: "https://kasbay.org",
+  logo: "https://cdn.jsdelivr.net/gh/KaspaHub/KaspaHub@latest/assets/logos/kasbay.webp",
+  features: {
+  }
+},
+{
+  name: "Burno",
+  officialSite: "https://burno.xyz/",
+  logo: "https://cdn.jsdelivr.net/gh/KaspaHub/KaspaHub@latest/assets/logos/burno.webp",
+  features: {
+    articles: true
+  }
+},
+{
+  name: "Kaspa FAQ",
+  officialSite: "https://www.kaspafaq.com/",
+  logo: "https://cdn.jsdelivr.net/gh/KaspaHub/KaspaHub@latest/assets/logos/kaspafaq.webp",
+  features: {
+    articles: true
+  }
+},
+{
+  name: "The Crypto Chronicles of Yonatan Sompolinsky",
+  officialSite: "https://nrcrowningshield.medium.com/the-crypto-chronicles-of-yonatan-sompolinsky-a-retelling-by-captain-sats-452c73077388",
+  logo: "https://cdn.jsdelivr.net/gh/KaspaHub/KaspaHub@latest/assets/logos/medium.webp",
+  features: {
+    articles: true
+  }
+},
+{
+  name: "Kaspa Hub - Articles",
+  officialSite: "https://kaspahub.org/articles/",
+  logo: "https://cdn.jsdelivr.net/gh/KaspaHub/KaspaHub@latest/assets/kaspahub.jpg",
+  features: {
+    articles: true
+  }
+},
+{
+  name: "KaspHub",
+  officialSite: "https://kasphub.com",
+  logo: "https://cdn.jsdelivr.net/gh/KaspaHub/KaspaHub@latest/assets/kaspa.webp",
+  features: {
+    articles: true,
+    analytics: true
+  }
+},
+{
+  name: "Kaspa Explorer",
+  officialSite: "https://explorer.kaspa.org",
+  logo: "https://cdn.jsdelivr.net/gh/KaspaHub/KaspaHub@latest/assets/logos/kaspaexplorer.webp",
+  features: {
+    blockExplorer: true
+  }
+},
+{
+  name: "Kas.fyi",
+  officialSite: "https://kas.fyi",
+  logo: "https://cdn.jsdelivr.net/gh/KaspaHub/KaspaHub@latest/assets/logos/kasfyi.webp",
+  features: {
+    analytics: true,
+    blockExplorer: true
+  }
+},
+{
+  name: "Kaspalytics",
+  officialSite: "https://www.kaspalytics.com",
+  logo: "https://cdn.jsdelivr.net/gh/KaspaHub/KaspaHub@latest/assets/kaspa.webp",
+  features: {
+    analytics: true,
+    visualizer: true
+  }
+},
+{
+  name: "Kaspa NG",
+  officialSite: "https://kaspa-ng.org",
+  logo: "https://cdn.jsdelivr.net/gh/KaspaHub/KaspaHub@latest/assets/logos/kaspang.webp",
+  features: {
+    visualizer: true
+  }
+},
+{
+  name: "KRC-20.org",
+  officialSite: "https://krc-20.org",
+  logo: "https://cdn.jsdelivr.net/gh/KaspaHub/KaspaHub@latest/assets/kaspa.webp",
+  features: {
+    analytics: true,
+  }
+},
+{
+  name: "KatScan",
+  officialSite: "https://katscan.xyz",
+  logo: "https://cdn.jsdelivr.net/gh/KaspaHub/KaspaHub@latest/assets/logos/katscan.webp",
+  features: {
+    analytics: true,
+    blockExplorer: true
+  }
+},
+{
+  name: "Kaspa Node Map",
+  officialSite: "https://nodes.kaspa.ws",
+  logo: "https://cdn.jsdelivr.net/gh/KaspaHub/KaspaHub@latest/assets/kaspa.webp",
+  features: {
+    analytics: true,
+    visualizer: true
+  }
+},
+{
+  name: "Kaspa Globe",
+  officialSite: "https://kaspaglo.be",
+  logo: "https://cdn.jsdelivr.net/gh/KaspaHub/KaspaHub@latest/assets/logos/kaspaglobe.webp",
+  features: {
+    analytics: true,
+    visualizer: true
+  }
+},
+{
+  name: "Kaspa Network Hashrate",
+  officialSite: "https://hashrate.kaspa.ws/",
+  logo: "https://cdn.jsdelivr.net/gh/KaspaHub/KaspaHub@latest/assets/kaspa.webp",
+  features: {
+    analytics: true,
+    visualizer: true
+  }
+},
+{
+  name: "Kasunder",
+  officialSite: "https://kasunder.com",
+  logo: "https://cdn.jsdelivr.net/gh/KaspaHub/KaspaHub@latest/assets/logos/kasunder.webp",
+  features: {
+    analytics: true,
+    visualizer: true
+  }
+},
+{
+  name: "Kaspa Graph Inspector",
+  officialSite: "https://kgi.kaspad.net",
+  logo: "https://cdn.jsdelivr.net/gh/KaspaHub/KaspaHub@latest/assets/kaspa.webp",
+  features: {
+    visualizer: true
+  }
+},
+{
+  name: "KasNodes",
+  officialSite: "https://kasnodes.com",
+  logo: "https://cdn.jsdelivr.net/gh/KaspaHub/KaspaHub@latest/assets/kaspa.webp",
+  features: {
+    analytics: true
+  }
+},
+{
+  name: "Kaspa Insights",
+  officialSite: "https://www.kaspainsights.com",
+  logo: "https://cdn.jsdelivr.net/gh/KaspaHub/KaspaHub@latest/assets/logos/kaspainsights.webp",
+  features: {
+    analytics: true
+  }
+},
+{
+  name: "Kaspa Rainbow Chart",
+  officialSite: "https://www.kasparainbowchart.com",
+  logo: "https://cdn.jsdelivr.net/gh/KaspaHub/KaspaHub@latest/assets/logos/kasparainbowchart.webp",
+  features: {
+    analytics: true,
+    visualizer: true
+  }
+},
+{
+  name: "KasMap",
+  officialSite: "https://kasmap.xyz/",
+  logo: "https://cdn.jsdelivr.net/gh/KaspaHub/KaspaHub@latest/assets/kaspa.webp",
+  features: {
+    visualizer: true
+  }
+},
+{
+  name: "Kasplore",
+  officialSite: "https://www.kasplore.com",
+  logo: "https://cdn.jsdelivr.net/gh/KaspaHub/KaspaHub@latest/assets/kaspa.webp",
+  features: {
+    blockExplorer: true,
+    visualizer: true
+  }
+},
+{
+  name: "Look into Kaspa",
+  officialSite: "https://www.lookintokaspa.com",
+  logo: "https://cdn.jsdelivr.net/gh/KaspaHub/KaspaHub@latest/assets/kaspa.webp",
+  features: {
+    analytics: true
+  }
+},
+{
+  name: "KaspaWave",
+  officialSite: "https://kaspawave.com",
+  logo: "https://cdn.jsdelivr.net/gh/KaspaHub/KaspaHub@latest/assets/kaspa.webp",
+  features: {
+    analytics: true
+  }
+},
+{
+  name: "KasView",
+  officialSite: "https://kasview.netlify.app",
+  logo: "https://cdn.jsdelivr.net/gh/KaspaHub/KaspaHub@latest/assets/kaspa.webp",
+  features: {
+    analytics: true
+  }
+},
+{
+  name: "Kaspa vs. Bitcoin Miner Revenue Calculator",
+  officialSite: "https://cryptoodie.github.io/KASPA-VS.-BITCOIN-MINER-REVENUE-/",
+  logo: "https://cdn.jsdelivr.net/gh/KaspaHub/KaspaHub@latest/assets/kaspa.webp",
+  features: {
+  }
+},
+{
+  name: "Kaspa Breakeven Calculator",
+  officialSite: "https://kas-breakeven-calc.vercel.app",
+  logo: "https://cdn.jsdelivr.net/gh/KaspaHub/KaspaHub@latest/assets/kaspa.webp",
+  features: {
+  }
+},
+{
+  name: "Kas Music 3D Visualizer",
+  officialSite: "https://kas-music.web.app",
+  logo: "https://cdn.jsdelivr.net/gh/KaspaHub/KaspaHub@latest/assets/kaspa.webp",
+  features: {
+    visualizer: true
+  }
+},
+{
+  name: "Kaspa 10 BPS 3D Visualizer",
+  officialSite: "https://kaspavisualizer.com",
+  logo: "https://cdn.jsdelivr.net/gh/KaspaHub/KaspaHub@latest/assets/kaspa.webp",
+  features: {
+    visualizer: true
+  }
+},
+{
+  name: "Kaspa GHOSTDAG Visualizer",
+  officialSite: "https://kaspa-gdpv.net",
+  logo: "https://cdn.jsdelivr.net/gh/KaspaHub/KaspaHub@latest/assets/kaspa.webp",
+  features: {
+    visualizer: true
+  }
+},
+{
+  name: "Kaspaspeed",
+  officialSite: "https://kaspaspeed.com/",
+  logo: "https://cdn.jsdelivr.net/gh/KaspaHub/KaspaHub@latest/assets/kaspa.webp",
+  features: {
+    visualizer: true
+  }
+},
+{
+  name: "Kaspa Network Visualizer",
+  officialSite: "https://macmachi.github.io/kaspa-network-visualizer",
+  logo: "https://cdn.jsdelivr.net/gh/KaspaHub/KaspaHub@latest/assets/kaspa.webp",
+  features: {
+    visualizer: true
+  }
+}
+];
