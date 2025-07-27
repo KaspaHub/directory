1d9f48e32e3907ba89eeb8f7297455b5fcdc80a3
Kaspa Hub <ivoclassic@pm.me>
2025-07-27 23:03:49 +0300

    Update wallets.js

--- wallets.js
+++ wallets.js
@@ -1,121 +1,121 @@
 export const wallets = [
 {
-item: "Tangem",
+title: "Tangem",
 link: "https://tangem.com",
 logo: "https://cdn.jsdelivr.net/gh/KaspaHub/directory@latest/logos/tangemwallet.webp"
 },
 {
-item: "OneKey",
+title: "OneKey",
 link: "https://onekey.so",
 logo: "https://cdn.jsdelivr.net/gh/KaspaHub/directory@latest/logos/onekeywallet.webp"
 },
 {
-item: "Kaspium",
+title: "Kaspium",
 link: "https://kaspium.io",
 logo: "https://cdn.jsdelivr.net/gh/KaspaHub/directory@latest/logos/kaspa.webp"
 },
 {
-item: "KasKeeper",
+title: "KasKeeper",
 link: "https://kaskeeper.vercel.app",
 logo: "https://cdn.jsdelivr.net/gh/KaspaHub/directory@latest/logos/kaskeeperwallet.webp"
 },
 {
-item: "Kasware",
+title: "Kasware",
 link: "https://www.kasware.xyz",
 logo: "https://cdn.jsdelivr.net/gh/KaspaHub/directory@latest/logos/kaswarewallet.webp"
 },
 {
-item: "Kastle Wallet",
+title: "Kastle Wallet",
 link: "https://kastle.cc",
 logo: "https://cdn.jsdelivr.net/gh/KaspaHub/directory@latest/logos/kastlewallet.webp"
 },
 {
-item: "Ghost Wallet",
+title: "Ghost Wallet",
 link: "https://www.ghostwallet.org",
 logo: "https://cdn.jsdelivr.net/gh/KaspaHub/directory@latest/logos/ghostwallet.webp"
 },
 {
-item: "Kurncy",
+title: "Kurncy",
 link: "https://www.kurncy.com",
 logo: "https://cdn.jsdelivr.net/gh/KaspaHub/directory@latest/logos/kurncywallet.webp"
 },
 {
-item: "Kaspa NG",
+title: "Kaspa NG",
 link: "https://kaspa-ng.org",
 logo: "https://cdn.jsdelivr.net/gh/KaspaHub/directory@latest/logos/kaspang.webp"
 },
 {
-item: "SafePal",
+title: "SafePal",
 link: "https://www.safepal.com",
 logo: "https://cdn.jsdelivr.net/gh/KaspaHub/directory@latest/logos/safepalwallet.webp"
 },
 {
-item: "Goldshell Wallet",
+title: "Goldshell Wallet",
 link: "https://www.goldshell.com/product/goldshell-wallet/",
 logo: "https://cdn.jsdelivr.net/gh/KaspaHub/directory@latest/logos/goldshellwallet.webp"
 },
 {
-item: "Cool Wallet",
+title: "Cool Wallet",
 link: "https://www.coolwallet.io",
 logo: "https://cdn.jsdelivr.net/gh/KaspaHub/directory@latest/logos/coolwallet.webp"
 },
 {
-item: "Kaspanet",
+title: "Kaspanet",
 link: "https://wallet.kaspanet.io",
 logo: "https://cdn.jsdelivr.net/gh/KaspaHub/directory@latest/logos/kaspa.webp"
 },
 {
-item: "Guarda",
+title: "Guarda",
 link: "https://guarda.com",
 logo: "https://cdn.jsdelivr.net/gh/KaspaHub/directory@latest/logos/guardawallet.webp"
 },
 {
-item: "Zelcore",
+title: "Zelcore",
 link: "https://zelcore.io",
 logo: "https://cdn.jsdelivr.net/gh/KaspaHub/directory@latest/logos/zelcorewallet.webp"
 },
 {
-item: "CoinEx Wallet",
+title: "CoinEx Wallet",
 link: "https://wallet.coinex.com",
 logo: "https://cdn.jsdelivr.net/gh/KaspaHub/directory@latest/logos/coinexwallet.webp"
 },
 {
-item: "OKX Wallet",
+title: "OKX Wallet",
 link: "https://web3.okx.com",
 logo: "https://cdn.jsdelivr.net/gh/KaspaHub/directory@latest/logos/okxwallet.webp"
 },
 {
-item: "NOW Wallet",
+title: "NOW Wallet",
 link: "https://walletnow.app",
 logo: "https://cdn.jsdelivr.net/gh/KaspaHub/directory@latest/logos/nowwallet.webp"
 },
 {
-item: "Plus Wallet",
+title: "Plus Wallet",
 link: "https://pluswallet.app",
 logo: "https://cdn.jsdelivr.net/gh/KaspaHub/directory@latest/logos/pluswallet.webp"
 },
 {
-item: "Atomik Wallet",
+title: "Atomik Wallet",
 link: "https://github.com/Atomik-Global/atomik-wallet",
 logo: "https://cdn.jsdelivr.net/gh/KaspaHub/directory@latest/logos/atomikwallet.webp"
 },
 {
-item: "Kaspacom Wallet",
+title: "Kaspacom Wallet",
 link: "https://github.com/KASPACOM/kaspacom-web-wallet",
 logo: "https://cdn.jsdelivr.net/gh/KaspaHub/directory@latest/logos/kaspacom.webp"
 },
 {
-item: "KSPR Bot",
+title: "KSPR Bot",
 link: "https://kspr-bot.xyz",
 logo: "https://cdn.jsdelivr.net/gh/KaspaHub/directory@latest/logos/ksprbot.webp"
 },
 {
-item: "Math Wallet",
+title: "Math Wallet",
 link: "https://mathwallet.org",
 logo: "https://cdn.jsdelivr.net/gh/KaspaHub/directory@latest/logos/mathwallet.webp"
 },
 {
-item: "Ledger",
+title: "Ledger",
 link: "https://kasvault.io",
 logo: "https://cdn.jsdelivr.net/gh/KaspaHub/directory@latest/logos/ledgerwallet.webp"
 }
