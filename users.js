450066785d4db790cd8683c0fa86b384687d9d4a
Kaspa Hub <ivoclassic@pm.me>
2025-07-29 20:16:27 +0300

    Update users.js

--- users.js
+++ users.js
@@ -5,6 +5,14 @@ wallet: "kaspa:qrv8305twny4434fjnsqvk2875tk3g27flz5akm49rzd7gpk0lrsus8y3ahwm",
 domain: "kas-hub.kas"
 },
 {
+x: "Classicgyt",
+wallet: "kaspa:qq4pv3qv64jjlg9sz20vgm0g933u3kfqx46td248g92sl6sjhwywum304f5al"
+},
+{
+x: "IzioDev",
+wallet: "kaspa:qpmwdyku2fcwzcahdzs86d0zz606vy7y6qxgrpkjxdz5xz8z6af9w2u6w0f3t"
+},
+{
 x: "KaspaSilver",
 wallet: "kaspa:qp408svlz585vyvj50yaljm8xdxrkcmmed8vxlx0wf0cl5wpt3vzyh74xs46e",
 domain: "kaspasilver.kas"
@@ -13,10 +21,6 @@ domain: "kaspasilver.kas"
 x: "Pumpolinsky",
 wallet: "kaspa:qr408tqlvgc75skyp3zztqrhkrdg2584qkzg30t9kt2nllryzzwxqx5t83q5w",
 domain: "pumpolinsky.kas"
-},
-{
-x: "IzioDev",
-wallet: "kaspa:qpmwdyku2fcwzcahdzs86d0zz606vy7y6qxgrpkjxdz5xz8z6af9w2u6w0f3t",
 },
 {
 x: "coinathlete",
